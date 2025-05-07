31204070c2a09dfbe473d19944e46fa7849502aa
unknown <zain2cool4school@gmail.com>
2025-05-07 19:05:39 +0100

    Just fixing some intial prisma bugs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,15 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-// Use global This to preserve the client across hot-reloads in development
-const globalForPrisma = (globalThis as any) as { prisma?: PrismaClient };
+// Extend the global namespace to include `prisma`
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
+// Use existing client instance in development (hot reloads), otherwise create a new one
+export const prisma = global.prisma ?? new PrismaClient({ log: ["query"] });
 
 if (process.env.NODE_ENV === "development") {
-  globalForPrisma.prisma = prisma;
+  global.prisma = prisma;
 }
