633840a3df5077faaf5cd88b5692496dcef299cd
unknown <zain2cool4school@gmail.com>
2025-05-08 18:17:31 +0100

    try last time

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -66,7 +66,7 @@ export default function SignupPage() {
 
     setLoading(true);
     try {
-      const res = await fetch("/api/signup", {
+      const res = await fetch("/app/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
