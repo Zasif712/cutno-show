321398e417091235f8fa47fc0138da6c026302c1
unknown <zain2cool4school@gmail.com>
2025-05-08 18:15:55 +0100

    please fix

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req: NextRequest) {
   // Parse and validate request body
   const {
